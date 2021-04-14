3a886cf0a13f527e604e5f3665550e9aaaa63b10
asd40441 <asd40441@hotmail.com>
2021-04-15 01:42:52 +0800

    test

--- Todolist/test.js
+++
@@ -1,7 +0,0 @@
-const title = document.querySelector('.box');
-
-title.addEventListener('click',function(e){
-    if(e.target.getAttribute('class') == 'view'){
-        console.log(e.target.getAttribute('value'));
-    }
-})
